ef579081dcecab5648c0ee373c7e1818adeec644
huimingz <kairu_madigan@yahoo.co.jp>
2019-07-05 15:26:18 +0900

    remove Fatal()

--- log.go
+++ log.go
@@ -12,7 +12,7 @@ type Logger interface {
 	Info(args ...interface{})
 	Warn(args ...interface{})
 	Error(args ...interface{})
-	Fatal(args ...interface{})
+	// Fatal(args ...interface{})
 	//
 	// Debugln(args ...interface{})
 	// Infoln(args ...interface{})
