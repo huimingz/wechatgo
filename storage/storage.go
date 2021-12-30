2a554796f8872efdf4834c4c0a3d0f38a1b64458
huimingz <huimingz12@outlook.com>
2021-12-30 11:08:54 +0800

    feat: update

--- storage/storage.go
+++ storage/storage.go
@@ -6,7 +6,12 @@ import (
 )
 
 type Storage interface {
+	// Get 获取Value
 	Get(key string) string
+
+	// Set 设置Value，包含TTL
 	Set(key, val string, ttl time.Duration) error
+
+	// HasExpired 检测是否过期
 	HasExpired(key string) bool
 }
