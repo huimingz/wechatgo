a75b3276b9c55f36092467d72f3a52a3e0de2acc
huimingz <huimingz12@outlook.com>
2022-03-29 11:37:14 +0800

    feat: 新增获取部门详情

--- wecom/dept/dept.go
+++ wecom/dept/dept.go
@@ -12,6 +12,7 @@ import (
 
 const (
 	urlDeptGet           = "/cgi-bin/department/list"
+	urlDeptDetailGet     = "/cgi-bin/department/get"
 	urlGetUserList       = "/cgi-bin/user/simplelist"
 	urlGetUserDetailList = "/cgi-bin/user/list"
 	urlDeptCreate        = "/cgi-bin/department/create"
@@ -30,6 +31,15 @@ type UserInfo struct {
 	UserId     string `json:"userid"`     // 成员UserID。对应管理端的帐号
 	Name       string `json:"name"`       // 成员名称
 	Department []int  `json:"department"` // 成员所属部门列表。列表项为部门ID，32位整型
+}
+
+type DepartmentDetail struct {
+	Id               int      `json:"id"`                // 部门id
+	Name             string   `json:"name"`              // 部门名称
+	NameEnglish      string   `json:"name_en"`           // 部门英文名称
+	DepartmentLeader []string `json:"department_leader"` // 部门领导人userid列表
+	ParentId         int      `json:"parentid"`          // 父部门id。根部门为1
+	Order            int      `json:"order"`             // 在父部门中的次序值。order值大的排序靠前。值范围是[0, 2^32)
 }
 
 type WechatDept struct {
@@ -55,6 +65,18 @@ func (w WechatDept) Get(ctx context.Context, deptId int) ([]DeptInfo, error) {
 	}{}
 	err := w.Client.Get(ctx, urlDeptGet, values, nil, &out)
 	return out.Department, err
+}
+
+// GetDetail 获取部门详情
+func (w WechatDept) GetDetail(ctx context.Context, deptId int) (*DepartmentDetail, error) {
+	values := url.Values{}
+	values.Add("id", strconv.Itoa(deptId))
+
+	out := struct {
+		Department DepartmentDetail `json:"department"`
+	}{}
+	err := w.Client.Get(ctx, urlDeptDetailGet, values, nil, &out)
+	return &out.Department, err
 }
 
 // 获取部门列表
