ab256e2a0f43404ed63de8340c2ffbc4a7ef990e
huimingz <huimingz12@outlook.com>
2023-03-09 20:50:52 +0800

    fix: client test issue

--- wecom/client_test.go
+++ wecom/client_test.go
@@ -1,6 +1,7 @@
 package wecom
 
 import (
+	"context"
 	"testing"
 
 	"github.com/huimingz/wechatgo/testdata"
@@ -14,36 +15,36 @@ var wechatClient = NewWechatClient(corpId, corpSecret, agentId)
 
 func TestWechatClient_IsExpired(t *testing.T) {
 	client := NewWechatClient("", "", 0)
-	if client.IsExpired() != true {
+	if client.IsExpired(context.Background()) != true {
 		t.Error("WechatClient.IsExpired() error = 'isExpired != true'")
 	}
 
-	_, _ = wechatClient.GetAccessToken()
-	if wechatClient.IsExpired() != false {
+	_, _ = wechatClient.GetAccessToken(context.Background())
+	if wechatClient.IsExpired(context.Background()) != false {
 		t.Error("WechatClient.IsExpired() error = 'isExpired != false'")
 	}
 }
 
 func TestWechatClient_GetAccessToken(t *testing.T) {
-	accessToken, _ := wechatClient.GetAccessToken()
+	accessToken, _ := wechatClient.GetAccessToken(context.Background())
 	if accessToken == "" {
 		t.Error("WechatClient.GetAccessToken() 返回结果access_token为空字符串")
 	}
 
 	// invalid client
 	client := NewWechatClient("xxx", corpSecret, agentId)
-	accessToken, _ = client.GetAccessToken()
+	accessToken, _ = client.GetAccessToken(context.Background())
 	if accessToken != "" {
 		t.Error("WechatClient.GetAccessToken() 返回结果access_token值为非空字符串")
 	}
 }
 
 func TestWechatClient_FetchAccessToken(t *testing.T) {
-	err := wechatClient.FetchAccessToken()
+	err := wechatClient.FetchAccessToken(context.Background())
 	if err != nil {
 		t.Errorf("WechatClient.FetchAccessToken() error = %s", err)
 	}
-	if token, _ := wechatClient.GetAccessToken(); token == "" {
+	if token, _ := wechatClient.GetAccessToken(context.Background()); token == "" {
 		t.Error("WechatClient.FetchAccessToken() accessToken值为空字符串")
 	}
 }
