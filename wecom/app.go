c9ba042021f6dac00ba862edfd6c697f017f8a8a
huimingz <huimingz12@outlook.com>
2023-03-18 19:24:40 +0800

    refactor: use pointer method

--- wecom/app.go
+++ wecom/app.go
@@ -27,7 +27,7 @@ func newWechatAppManage(client *WechatClient) *applicationManager {
 // 获取access_token对应的应用列表
 //
 // 参考文档：https://work.weixin.qq.com/api/doc#90000/90135/90227
-func (w applicationManager) GetAllApp(ctx context.Context) ([]AppIntro, error) {
+func (w *applicationManager) GetAllApp(ctx context.Context) ([]AppIntro, error) {
 	agentList := struct {
 		AgentList []AppIntro `json:"agentlist"`
 	}{}
@@ -38,12 +38,9 @@ func (w applicationManager) GetAllApp(ctx context.Context) ([]AppIntro, error) {
 // 获取指定的应用详情
 //
 // 参考文档：https://work.weixin.qq.com/api/doc#90000/90135/90227
-func (w applicationManager) GetApp(ctx context.Context, agentId int) (*AppDetail, error) {
+func (w *applicationManager) GetApp(ctx context.Context, agentId int) (*AppDetail, error) {
 	values := url.Values{}
-	if agentId == 0 {
-		agentId = w.client.AgentId
-	}
-	values.Add("agentid", strconv.Itoa(agentId))
+	values.Add("agentid", w.toStringAgentId(w.orClientAgentId(agentId)))
 
 	appDetail := AppDetail{}
 	err := w.client.Get(ctx, urlGetApp, values, nil, &appDetail)
@@ -53,22 +50,16 @@ func (w applicationManager) GetApp(ctx context.Context, agentId int) (*AppDetail
 // 设置应用
 //
 // 参考文档：https://work.weixin.qq.com/api/doc#90000/90135/90228
-func (w applicationManager) CreateApp(ctx context.Context, appInfo AppInfo) error {
+func (w *applicationManager) CreateApp(ctx context.Context, appInfo AppInfo) error {
 	return w.client.Post(ctx, urlCreateApp, nil, appInfo, nil, nil)
 }
 
 // 创建菜单
 //
 // 参考资料：https://work.weixin.qq.com/api/doc#90000/90135/90230
-func (w applicationManager) CreateMenu(ctx context.Context, menu Menu, agentId int) error {
+func (w *applicationManager) CreateMenu(ctx context.Context, menu Menu, agentId int) error {
 	values := url.Values{}
-	if agentId == 0 {
-		agentId = w.client.AgentId
-	}
-	if agentId == 0 {
-		agentId = w.client.AgentId
-	}
-	values.Add("agentid", strconv.Itoa(agentId))
+	values.Add("agentid", w.toStringAgentId(w.orClientAgentId(agentId)))
 
 	return w.client.Post(ctx, urlCreateMenu, values, menu, nil, nil)
 }
@@ -76,12 +67,9 @@ func (w applicationManager) CreateMenu(ctx context.Context, menu Menu, agentId i
 // 获取菜单
 //
 // 参考文档：https://work.weixin.qq.com/api/doc#90000/90135/90232
-func (w applicationManager) GetMenu(ctx context.Context, agentId int) (*Menu, error) {
+func (w *applicationManager) GetMenu(ctx context.Context, agentId int) (*Menu, error) {
 	values := url.Values{}
-	if agentId == 0 {
-		agentId = w.client.AgentId
-	}
-	values.Add("agentid", strconv.Itoa(agentId))
+	values.Add("agentid", w.toStringAgentId(w.orClientAgentId(agentId)))
 
 	menu := Menu{}
 	err := w.client.Get(ctx, urlGetMenu, values, nil, &menu)
@@ -91,12 +79,20 @@ func (w applicationManager) GetMenu(ctx context.Context, agentId int) (*Menu, er
 // 删除菜单
 //
 // 参考文档：https://work.weixin.qq.com/api/doc#90000/90135/90233
-func (w applicationManager) DeleteMenu(ctx context.Context, agentId int) error {
+func (w *applicationManager) DeleteMenu(ctx context.Context, agentId int) error {
 	values := url.Values{}
-	if agentId == 0 {
-		agentId = w.client.AgentId
-	}
-	values.Add("agentid", strconv.Itoa(agentId))
+	values.Add("agentid", w.toStringAgentId(w.orClientAgentId(agentId)))
 
 	return w.client.Get(ctx, urlDelMenu, values, nil, nil)
+}
+
+func (w *applicationManager) orClientAgentId(agentId int) int {
+	if agentId == 0 {
+		return w.client.AgentId
+	}
+	return agentId
+}
+
+func (w *applicationManager) toStringAgentId(agentId int) string {
+	return strconv.Itoa(w.orClientAgentId(agentId))
 }
